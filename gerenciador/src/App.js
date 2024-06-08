4add85ba9ccba9000db47fe73da54b96d49225f9
devdinei <devdinei@gmail.com>
2024-06-08 15:22:21 -0300

    Ultimos Exercicios

--- gerenciador/src/App.js
+++ gerenciador/src/App.js
@@ -1,12 +1,22 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 
 function App() {
-
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/empresa' element={<Empresa />} />
+        <Route path='/contato' element={<Contato />} />
+      </Routes>
+      <Footer />
+    </Router>  
   );
 }
 
